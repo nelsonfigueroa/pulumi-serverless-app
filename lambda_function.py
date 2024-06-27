0edb04a80e1ba16470f83ddc674e65926106cbe4
nelsonfigueroa <30811275+nelsonfigueroa@users.noreply.github.com>
2024-06-27 16:42:51 -0700

    removing some old comments

--- lambda_function.py
+++ lambda_function.py
@@ -7,10 +7,6 @@ TABLE_NAME = "Uploads"
 def lambda_handler(event, context):
     client = boto3.client("dynamodb", "us-west-1")
 
-    # need to get file_name and add a timestamp, which should be generated on the fly. Unless its provided in the payload?
-    # body = json.loads(event["body"])
-    # event = json.loads(event)
-
     print(event)
     print(type(event))
 
